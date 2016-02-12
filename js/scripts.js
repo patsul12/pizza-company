2cf8e73ec11556f6b463a9687b4c300d2d48369c
patsul12 <patsul12@gmail.com>
2016-02-12 08:09:27 -0800

    alter the way pizza cost is calculated

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,12 @@ function Pizza(size, toppings) {
 
 Pizza.prototype.cost = function() {
   var salesTax = 0.05;
-  var priceBySize = new Object();
-  priceBySize["small"] = 10.00;
-  priceBySize["medium"] = 12.00;
-  priceBySize["large"] = 14.00;
-  priceBySize["xl"] = 16.00;
+  var priceBySize = {
+    small: 10.00,
+    medium: 12.00,
+    large: 14.00,
+    xl: 16.00
+  }
 
   return (priceBySize[this.size] + this.toppings.length * .50) * (1 + salesTax);
 };
