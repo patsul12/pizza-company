497d538187337d0b4a7ed1df36e0b2b693b22848
patsul12 <patsul12@gmail.com>
2016-02-12 08:07:23 -0800

    add ability to calculate the cost of a given pizza

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
 }
+
+Pizza.prototype.cost = function() {
+  var salesTax = 0.05;
+  var priceBySize = new Object();
+  priceBySize["small"] = 10.00;
+  priceBySize["medium"] = 12.00;
+  priceBySize["large"] = 14.00;
+  priceBySize["xl"] = 16.00;
+
+  return (priceBySize[this.size] + this.toppings.length * .50) * (1 + salesTax);
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,8 +1,13 @@
 describe('Pizza', function() {
+  var toppings = ["cheese", "pepperoni", "salami", "mushrooms"]
+  var testPizza = new Pizza("large", toppings);
+
   it("creates a pizza with given size and toppings", function() {
-    var toppings = ["cheese", "pepperoni", "salami", "mushrooms"]
-    var testPizza = new Pizza("large", toppings);
     expect(testPizza.size).to.equal("large");
     expect(testPizza.toppings).to.eql(toppings)
+  });
+
+  it("calculates the cost of a pizza", function() {
+    expect(testPizza.cost()).to.equal(16.80)
   });
 })
